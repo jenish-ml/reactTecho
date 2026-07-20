906630c006e992117dd229b5daf018c107527995
jenish-ml <jenishpunjiri@gmail.com>
2026-07-20 11:45:01 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card"
+import Footer from "./Footer"
+import Header from "./Header"
+import ClassExample from "./LearnComponents/ClassExample"
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Card />
+    <Footer />
+    <ClassExample />
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  const name = "JENISH"
+  return (
+    <>
+        <h1 style={{ color: 'blue', textAlign: 'center' }}>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nihil blanditiis repellat perferendis, veritatis a quibusdam reprehenderit! Aliquam, iure amet vel dolorum hic minima alias tempora voluptatum consequuntur facere ullam, molestiae voluptas delectus iste esse repellendus eveniet dolores, ipsum natus consectetur mollitia velit dolor saepe eligendi? Nihil alias unde similique laboriosam illum rerum. Rem, sed neque. Quasi ipsam molestias voluptatum assumenda cumque nam illo delectus incidunt deserunt adipisci repellat laudantium eaque, nemo repellendus est vel quibusdam laboriosam optio officiis quaerat voluptatibus earum commodi? Libero labore, dignissimos non sequi ea voluptas suscipit, repellendus qui incidunt quae cum necessitatibus rerum odit omnis!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nihil blanditiis repellat perferendis, veritatis a quibusdam reprehenderit! Aliquam, iure amet vel dolorum hic minima alias tempora voluptatum consequuntur facere ullam, molestiae voluptas delectus iste esse repellendus eveniet dolores, ipsum natus consectetur mollitia velit dolor saepe eligendi? Nihil alias unde similique laboriosam illum rerum. Rem, sed neque. Quasi ipsam molestias voluptatum assumenda cumque nam illo delectus incidunt deserunt adipisci repellat laudantium eaque, nemo repellendus est vel quibusdam laboriosam optio officiis quaerat voluptatibus earum commodi? Libero labore, dignissimos non sequi ea voluptas suscipit, repellendus qui incidunt quae cum necessitatibus rerum odit omnis!</p>
+    </>
+  )
+}
+
+export default Card
