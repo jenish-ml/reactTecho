ff1b0bae4818a6c4566ff64fbe331bad67a1b06f
jenish-ml <jenishpunjiri@gmail.com>
2026-07-23 10:27:00 +0530

    Changed Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import Card from "./Card"
-import Footer from "./Footer"
-import Header from "./Header"
-import ClassExample from "./LearnComponents/ClassExample"
-
 
 function App() {
   return (
     <>
-    <Header />
-    <Card />
-    <Footer />
-    <ClassExample />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,6 @@ import MyApp from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MyApp />
+     <MyApp />
   </StrictMode>,
 )
